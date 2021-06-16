0b2d1b4e22654f698c11e530ee3977d87cf8d04d
nj1225 <83825494+nj1225@users.noreply.github.com>
2021-06-16 20:14:09 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Madlibs
+ It is a guessing word game.Just for fun.
---
+++ main.py
@@ -0,0 +1,48 @@
+print("Do as little ___  to others as you can; make any ___ for your ___ friends; be ___ for yourself and ask nothing of others; and ___ all the fun you can. Don't give much ___ to yesterday, don't worry about tomorrow, ___ in the moment, and ___ that your existence has meaning even when the world seems to be all ___ chance and chaos. When life lands a hammer ___ in your face, do your best to ___ to the hammer as if it had been a cream pie.")
+
+
+
+
+
+
+adj1 = input("Adjective:negative word ")
+adj2 = input("Adjective:positive word ")
+adj3 = input("Adjective:positive word ")
+adj5 = input("Adjective:positive word ")
+verb1 = input("verb: ")
+verb3 = input("verb: ")
+
+adj4 = input("Adjective:positive word ")
+adj7 = input("Adjective:positive word ")
+adj6 = input("Adjective:negative word ")
+
+verb2 = input("verb: ")
+verb4 = input("verb: ")
+
+
+
+
+madlib = f"Do as little {adj1}  to others as you can; make any {adj2} for your {adj3} friends; be {adj5} for yourself and ask nothing of others; and {verb1} all the fun you can. Don't give much {verb3} to yesterday, don't worry about tomorrow, {adj4} in the moment, and {adj7} that your existence has meaning even when the world seems to be all {adj6} chance and chaos. When life lands a hammer {verb2} in your face, do your best to {verb4} to the hammer as if it had been a cream pie."
+
+print(madlib ) 
+print("\n")
+print ("As the sun __ the day and the moon __ the night, so too, we are connected by: the air that we __, light that we see and the darkness that __. Life is too short to __ it on __. Surely, we can all __ to disagree. So let us find a __ ground, form a __ and spread joy, happiness and freedom around the world for the __ of you, me and the __ generations to come.")
+
+
+verb5 = input("verb: ")
+verb6 = input("verb: ")
+verb7 = input("verb: ")
+verb8 = input("verb: ")
+adj8 = input("Adjective:negative word ")
+adj9 = input("Adjective:negative word ")
+verb9 = input("verb: ")
+adj10 = input("Adjective:positive word ")
+noun = input("noun: ")
+adj11 = input("Adjective:positive word ")
+adj12 = input("Adjective:positive word ")
+
+
+
+madlib_second = f"As the sun {verb5} the day and the moon {verb6} the night, so too, we are connected by: the air that we {verb7}, light that we see and the darkness that {verb8}. Life is too short to {adj8} it on {adj9}. Surely, we can all {verb9} to disagree. So let us find a {adj10} ground, form a {noun} and spread joy, happiness and freedom around the world for the {adj11} of you, me and the {adj12} generations to come."
+
+print(madlib_second)
